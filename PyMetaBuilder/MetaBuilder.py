97caf69ac7ced1ca20418845e5291ac34864de5c
ebossi <ernesto@localhost.localdomain>
2013-05-29 20:43:13 -0300

    made it PEP8 compilant

--- PyMetaBuilder/MetaBuilder.py
+++ PyMetaBuilder/MetaBuilder.py
@@ -1,24 +1,29 @@
-from types import MethodType,ModuleType
+from types import MethodType, ModuleType
 import string
 
-def getMethodsByName(obj,name):
+
+def getMethodsByName(obj, name):
     return [method for method in getMethods(obj) if name in method]
 
-def get_class( klass ):
+
+def get_class(klass):
     parts = klass.split('.')
     module = ".".join(parts[:-1])
-    m = __import__( module )
+    m = __import__(module)
     for comp in parts[1:]:
         m = getattr(m, comp)
     return m
 
-def bind(f,obj):
-    obj.__dict__[f.__name__]=MethodType(f,obj,obj.__class__)
 
-rebind= lambda f,obj: bind(unbind(f),obj)
+def bind(f, obj):
+    obj.__dict__[f.__name__] = MethodType(f, obj, obj.__class__)
+
+rebind = lambda f, obj: bind(unbind(f), obj)
+
 
 def getMethods(obj):
-    ret=[]
+
+    ret = []
     for e in dir(obj):
         try:
             if callable(getattr(obj, e)):
@@ -27,28 +32,32 @@ def getMethods(obj):
             pass
     return ret
 
-def isAttributeDefined(obj,attribute):
-    if hasattr(obj,attribute)  and getattr(obj,attribute) is not None:
+
+def isAttributeDefined(obj, attribute):
+    if hasattr(obj, attribute) and getattr(obj, attribute) is not None:
         return
     raise AttributeError('Attribute {0} is not defined'.format(attribute))
 
+
 def getAttributes(obj):
-    return [prop for (prop,value) in vars(obj).iteritems()]
+    return [prop for (prop, value) in vars(obj).iteritems()]
+
 
 def createvar_if_not_exists(obj, var, initial):
     try:
-        getattr(obj,var)
+        getattr(obj, var)
     except AttributeError:
         setattr(obj, var, initial)
 
+
 def unbind(f):
     self = getattr(f, '__self__', None)
-    if self is not None and not isinstance(self, ModuleType)\
-    and not isinstance(self, type):
+    if self is not None and not isinstance(self, ModuleType) and not isinstance(self, type):
         if hasattr(f, '__func__'):
             return f.__func__
         return getattr(type(f.__self__), f.__name__)
     raise TypeError('not a bound method')
+
 
 class MetaBuilder(object):
 
@@ -57,149 +66,133 @@ class MetaBuilder(object):
         self.validators = self._get_Validators()
         self.callbacks = dict()
         for v in self.validators:
-            self.callbacks[v.split(self.prefix)[1]]=getattr(self,v)
+            self.callbacks[v.split(self.prefix)[1]] = getattr(self, v)
 
     #Validators
     def _get_Validators(self):
         return getMethodsByName(self, self.prefix)
 
-    def _getValidatorsByName(self,name):
-        return [getattr(self,val) for val in self._get_Validators() if name in val]
+    def _getValidatorsByName(self, name):
+        return [getattr(self, val) for val in self._get_Validators() if name in val]
 
-    def validate_type (self,value,expected_type):
+    def validate_type(self, value, expected_type):
         if not type(value) in ([expected_type]):
-            raise TypeError,"Should be of type {0}".format(expected_type)
+            raise TypeError("Should be of type {0}".format(expected_type))
 
-    def validate_one_of(self,value,options):
+    def validate_one_of(self, value, options):
         if value not in options:
-            raise OptionValueError,"Value {0} not in expected options".format(value)
+            raise OptionValueError("Value {0} not in expected options".format(value))
 
-    def validate_validates(self,value,method):
-        return self.customMethodValidator(value,method)
+    def validate_validates(self, value, method):
+        return self.customMethodValidator(value, method)
 
-    def customMethodValidator(self,value,method):
+    def customMethodValidator(self, value, method):
         if not callable(method):
-            raise TypeError,"{0} is not a method or callable one".format(method)
+            raise TypeError("{0} is not a method or callable one".format(method))
         try:
             if not method(value):
-                raise ValidatorError,"Value {0} did not passed validation {1}".format(value,method)
-        except TypeError,e:
-            raise ValidatorError,e,'Problem calling method {0}'.format(method)
+                raise ValidatorError("Value {0} did not passed validation {1}".format(value, method))
+        except TypeError, e:
+            raise ValidatorError(e, 'Problem calling method {0}'.format(method))
 
-    def required(self,*args,**kwargs):
-        self._required_args=[]
+    def required(self, *args, **kwargs):
+        self._required_args = []
         for arg in args:
             self._required_args.append(arg)
 
-    def model(self,klass):
-        createvar_if_not_exists(self,"_model",klass)
-        self._model=klass
+    def model(self, klass):
+        createvar_if_not_exists(self, "_model", klass)
+        self._model = klass
 
-    def property(self,attribute,*args,**kwargs):
+    def property(self, attribute, *args, **kwargs):
         """
         self.property('age',type=int)
         """
-        callback=self.getCallback(*args,**kwargs)
+        callback = self.getCallback(*args, **kwargs)
         if callback:
-            callbackName=callback.__name__+'_'+attribute
-            self.__dict__[callbackName]=MethodType(unbind(callback),self)
-            callbackarg=self.getCallbackArg(callbackName,*args,**kwargs)
+            callbackName = callback.__name__+'_'+attribute
+            self.__dict__[callbackName] = MethodType(unbind(callback), self)
+            callbackarg = self.getCallbackArg(callbackName, *args, **kwargs)
             #create setter and getters
-            self.buildProperty(attribute,callbackName,callbackarg)
+            self.buildProperty(attribute, callbackName, callbackarg)
         else:
-            self.buildProperty(attribute,None,None)
+            self.buildProperty(attribute, None, None)
 
-    def buildProperty(self,attributeName,callbackName=None,callbackarg=None):
-        getter=self.buildGetter(attributeName)
-        setter=self.buildSetter(attributeName,callbackName,callbackarg)
-        self.setProperty(self,attributeName,getter,setter,None)
+    def buildProperty(self, attributeName, callbackName=None, callbackarg=None):
+        getter = self.buildGetter(attributeName)
+        setter = self.buildSetter(attributeName, callbackName, callbackarg)
+        self.setProperty(self, attributeName, getter, setter, None)
 
-    def setProperty(self,obj,attributeName,getter,setter,defaultValue=None):
-        setattr(obj.__class__,attributeName,property(fget=getter,fset=setter))
+    def setProperty(self, obj, attributeName, getter, setter, defaultValue=None):
+        setattr(obj.__class__, attributeName, property(fget=getter, fset=setter))
         setattr(obj, '_'+attributeName, defaultValue)
 
-    def getSignatureString(self,methodString):
+    def getSignatureString(self, methodString):
         return methodString[methodString.find("def")+3:methodString.find("(")].strip()
 
-    def _getAttrName(self,propertyName):
+    def _getAttrName(self, propertyName):
         return '_'+propertyName
 
-    def buildGetter(self,propertyName):
-        getter="def get{0}(self):\n" \
-               "    return self.{1}".format(propertyName,self._getAttrName(propertyName))
-        return self.createFunction(self,getter)
+    def buildGetter(self, propertyName):
+        getter = "def get{0}(self):\n" \
+                 "    return self.{1}".format(propertyName, self._getAttrName(propertyName))
+        return self.createFunction(self, getter)
 
-    def buildSetter(self,propertyName,callbackName=None,callbackarg=None):
-        callback='' if callbackName is None else 'self.{0}(value,{1})'.format(callbackName,callbackarg)
-        setter="def set{0}(self,value):\n" \
-               "    {1}\n" \
-               "    self.{2}=value".format(propertyName,callback.strip(),self._getAttrName(propertyName))#Armar un CodeType antes de armar el FunctionType
-        return self.createFunction(self,setter)
+    def buildSetter(self, propertyName, callbackName=None, callbackarg=None):
+        callback = '' if callbackName is None else 'self.{0}(value,{1})'.format(callbackName, callbackarg)
+        setter = "def set{0}(self,value):\n" \
+                 "    {1}\n" \
+                 "    self.{2}=value".format(propertyName, callback.strip(), self._getAttrName(propertyName))
+        return self.createFunction(self, setter)
 
-    def createFunction(self,klass,code):
+    def createFunction(self, klass, code):
         dict = {}
-        methodName=self.getSignatureString(code)
+        methodName = self.getSignatureString(code)
         exec code.strip() in dict
-        klass.__dict__[methodName]=dict[methodName]
+        klass.__dict__[methodName] = dict[methodName]
         return klass.__dict__[methodName]
 
-    def getCallback(self,*args,**kwargs):
+    def getCallback(self, *args, **kwargs):
         for kwarg,validateArg in kwargs.iteritems():
-            for callbackname,callback in self.callbacks.iteritems():
-                if callbackname==kwarg:
+            for callbackname, callback in self.callbacks.iteritems():
+                if callbackname == kwarg:
                     return callback
             return None
 
-    def getCallbackArg(self,callbackName,*args,**kwargs):
-        argumentName,argument=kwargs.popitem()
-        _name=['type','validates']
+    def getCallbackArg(self, callbackName, *args, **kwargs):
+        argumentName, argument = kwargs.popitem()
+        _name = ['type', 'validates']
         for n in _name:
             if n in callbackName:
                 return argument.__name__
         return argument
 
     def getProperties(self):
-        start=['_model','callbacks','validators','_required_args','prefix']
-        return [string.replace(k,'_','') for k in getAttributes(self) if k not in getMethods(self)+start]
+        start = ['_model', 'callbacks', 'validators', '_required_args', 'prefix']
+        return [string.replace(k, '_', '') for k in getAttributes(self) if k not in getMethods(self)+start]
 
     def build(self):
         for required in self._required_args:
-            isAttributeDefined(self,required)
-        klass=get_class(self._model.__module__+'.'+self._model.__name__)
-        instance=klass()
+            isAttributeDefined(self, required)
+        klass = get_class(self._model.__module__+'.'+self._model.__name__)
+        instance = klass()
         for prop in self.getProperties():
-            for meth in [getattr(self,m) for m in getMethodsByName(self,prop)]:
-                if self.prefix in meth.__name__:
-                    instance.__dict__[meth.__name__+'_'+prop]=MethodType(unbind(meth),self)
+            for method in [getattr(self, m) for m in getMethodsByName(self, prop)]:
+                if self.prefix in method.__name__:
+                    instance.__dict__[method.__name__+'_'+prop] = MethodType(unbind(method), self)
                 else:
-                    instance.__dict__[meth.__name__]=self.__dict__[meth.__name__]
-            getter=getattr(instance,'get{0}'.format(prop))
-            setter=getattr(instance,'set{0}'.format(prop))
-            self.setProperty(instance,prop,getter,setter,getattr(self,'_'+prop))
+                    instance.__dict__[method.__name__] = self.__dict__[method.__name__]
+            getter = getattr(instance, 'get{0}'.format(prop))
+            setter = getattr(instance, 'set{0}'.format(prop))
+            self.setProperty(instance, prop, getter, setter, getattr(self, '_'+prop))
         return instance
+
 
 class OptionValueError(StandardError):
     def __init__(self, *args, **kwargs):
         StandardError.__init__(self, *args, **kwargs)
 
+
 class ValidatorError(StandardError):
     def __init__(self, *args, **kwargs):
         StandardError.__init__(self, *args, **kwargs)
-
-class A(object):
-    pass
-
-class MyMeta(MetaBuilder):
-    pass
-
-if __name__=='__main__':
-    a=MyMeta()
-    a.model(A)
-    a.property('saraza',one_of=["doctor", "musician"])
-    a.required('saraza')
-    a.saraza="doctor"
-    isAttributeDefined(a,'saraza')
-    ainst=a.build()
-    del a
-    ainst.saraza="sician"
-    print ainst._saraza
--- test/testMetaBuilder.py
+++ test/testMetaBuilder.py
@@ -1,8 +1,10 @@
-from unittest import TestCase,skip
+from unittest import TestCase, skip
 from PyMetaBuilder import MetaBuilder
+
 
 class Person(object):
     pass
+
 
 class PersonMetaBuilder(MetaBuilder.MetaBuilder):
 
@@ -10,32 +12,33 @@ class PersonMetaBuilder(MetaBuilder.MetaBuilder):
         MetaBuilder.MetaBuilder.__init__(self)
         self.model(Person)
         self.property('name')
-        self.property('age',type=int)
-        self.property('job',one_of=["doctor", "musician"])
-        self.property('height',validates=self.myvalidator)
+        self.property('age', type=int)
+        self.property('job', one_of=["doctor", "musician"])
+        self.property('height', validates=self.myvalidator)
         self.required('name')
 
-    def myvalidator(self,value):
+    def myvalidator(self, value):
         pass
+
 
 class PyMetaBuilderTest(TestCase):
 
     def setUp(self):
-        self.personMeta=PersonMetaBuilder()
-        self.personMeta=PersonMetaBuilder()
+        self.personMeta = PersonMetaBuilder()
+        self.personMeta = PersonMetaBuilder()
 
     def testHierarchy(self):
-        self.assertIsInstance(self.personMeta,MetaBuilder.MetaBuilder)
+        self.assertIsInstance(self.personMeta, MetaBuilder.MetaBuilder)
 
     def testShouldAbleToAddModel(self):
-        self.assertEqual(Person,self.personMeta.__getattribute__("_model"))
+        self.assertEqual(Person, self.personMeta.__getattribute__("_model"))
 
     def testCheckPropertyset(self):
-        self.assertIn('validate_type_age',MetaBuilder.getMethods(self.personMeta))
+        self.assertIn('validate_type_age', MetaBuilder.getMethods(self.personMeta))
 
     def testGetValidatorsName(self):
-        val=self.personMeta._getValidatorsByName('age')
-        self.assertEqual('validate_type',val[0].__name__)
+        val = self.personMeta._getValidatorsByName('age')
+        self.assertEqual('validate_type', val[0].__name__)
 
     def testAttributes(self):
         self.assertTrue('name' in self.personMeta.getProperties())
@@ -44,31 +47,31 @@ class PyMetaBuilderTest(TestCase):
         self.assertTrue('height' in self.personMeta.getProperties())
 
     def testCorrectAttributeType(self):
-        self.personMeta.age=50
-        self.assertEqual(50,self.personMeta.age)
+        self.personMeta.age = 50
+        self.assertEqual(50, self.personMeta.age)
 
     def testIncorrectAttributeType(self):
         def setAge():
-            self.personMeta.age='ssse'
-        self.assertRaises(TypeError,setAge)
+            self.personMeta.age = 'ssse'
+        self.assertRaises(TypeError, setAge)
 
     def testCorrectAttributeOptions(self):
-        self.personMeta.job="doctor"
-        self.assertEqual("doctor",self.personMeta.job)
+        self.personMeta.job = "doctor"
+        self.assertEqual("doctor", self.personMeta.job)
 
     def testIncorrectAttributeOptions(self):
         def setJob():
-            self.personMeta.job='ssse'
-        self.assertRaises(MetaBuilder.OptionValueError,setJob)
+            self.personMeta.job = 'ssse'
+        self.assertRaises(MetaBuilder.OptionValueError, setJob)
 
     def testRequiredNotFilled(self):
         def buildWithouthRequired():
-            self.personMeta.age=20
+            self.personMeta.age = 20
             self.personMeta.build()
-        self.assertRaises(AttributeError,buildWithouthRequired)
+        self.assertRaises(AttributeError, buildWithouthRequired)
 
     def test_build(self):
-        self.personMeta.age=50
-        self.personMeta.name='Jhon Doe'
-        instance=self.personMeta.build()
-        self.assertIsInstance(instance,Person)
+        self.personMeta.age = 50
+        self.personMeta.name = 'Jhon Doe'
+        instance = self.personMeta.build()
+        self.assertIsInstance(instance, Person)
