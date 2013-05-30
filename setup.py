0d1b5f7bd973b8d235ba990d17a6aa5a9de7f4cd
bossiernesto <bossi.ernestog@gmail.com>
2013-05-30 00:32:49 -0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from distutils.core import setup
 
 setup(name="PyMetaBuilder",
       version="0.1",
-      description="",
+      description="Small framework for creating Builders and entities",
       author="Ernesto Bossi",
       author_email="bossi.ernestog@gmail.com",
       url="https://github.com/bossiernesto/PyMetaBuilder",
-      license="GPL",
-      py_modules=["PyMetaBuilder"]
+      license="GPL v3",
+      py_modules=["PyMetaBuilder"],
+      keywords="MetaBuilder Metaprogramming",
+      classifiers=["Development Status :: 3 - Alpha",
+                   "Topic :: Utilities",
+                   "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)"]
 )
 
